79f56b2068ada21ec925f23de7ce53301d3cba5d
ovarz <ovarz_03@yahoo.com>
2021-01-19 02:01:11 +0700

    update base

--- inc/footer.php
+++ inc/footer.php
@@ -1,34 +1,63 @@
 <footer class="content_center">
   <span>
-      <div class="footer-info-desc">
-        <p>Handal Semesta Berjangka memiliki izin operasional yang legal dan telah disahkan oleh Badan Pengawas Perdagangan Berjangka Komiditi (BAPPEBTI) Kementerian Perdagangan Republik Indonesia selaku Badan Otoritas Perdagangan Berjangka Komoditi dan Derivative di Indonesia.</p>
-        <p>Handal Semesta Berjangka juga merupakan suatu Perusahaan Pialang Berjangka yang resmi diakui oleh Pemerintah Republik Indonesia dan menjadi Anggota Bursa Indonesia Commodity & Derivatives Exchange (ICDX).</p>
-        <p>Handal Semesta Berjangka juga telah menjadi Anggota Kliring yang resmi dari Indonesia Clearing House (ICH) sebagai Lembaga Kliring (Penjamin).</p>
-        <h5>PERINGATAN RISIKO</h5>
-        <p>Produk dengan leverage memiliki tingkat risiko yang tinggi terhadap modal yang anda investasikan dan disarankan hanya menggunakan dana yang mampu anda tanggung apabila terjadi kerugian. Nilai investasi dapat turun atau naik dan Anda dapat kehilangan pembayaran margin awal anda. Harap diketahui bahwa produk dengan leverage belum tentu cocok untuk semua orang, jadi pastikan anda telah memahami sepenuhnya semua risiko yang terlibat.</p>
+    <div class="footer-office">
+      <div class="footer-office-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.3812318383425!2d106.81810631529505!3d-6.213348662590675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5962be3c2bf%3A0xa6e1ba6ecaf76c3e!2sHSB%20Investasi!5e0!3m2!1sen!2sid!4v1610989996304!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       </div>
-      <div class="footer-info-office">
-        <div class="fio-map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.3812318383425!2d106.81810631529505!3d-6.213348662590675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5962be3c2bf%3A0xa6e1ba6ecaf76c3e!2sHSB%20Investasi!5e0!3m2!1sen!2sid!4v1610989996304!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-        </div>
-        <div class="fio-addr">
+      <div class="footer-office-info">
+        <div class="footer-addr">
           <b>HANDAL SEMESTA BERJANGKA</b>
           <p>Mayapada Tower 2 Lt.14,</p>
           <p>Jln. Jend. Sudirman Kav. 27 RT.4/RW.2, Karet, Kec Setiabudi,</p>
           <p>Jakarta Selatan 12920</p>
         </div>
-        <a aria-label="Link Title" alt="link_title" class="fio-contact" href="tel:02150595588" target="_blank">
-          <div class="fio-contact-icon"><i class="fas fa-phone"></i></div>
-          <div class="fio-contact-data">+62 21 5059 5588</div>
-        </a>
-        <a aria-label="Link Title" alt="link_title" class="fio-contact" href="www.hsb.co.id" target="_blank">
-          <div class="fio-contact-icon"><i class="fas fa-globe"></i></div>
-          <div class="fio-contact-data">www.hsb.co.id</div>
-        </a>
-        <a aria-label="Link Title" alt="link_title" class="fio-contact" href="mailto:info@hsb.co.id" target="_blank">
-          <div class="fio-contact-icon"><i class="fas fa-envelope"></i></div>
-          <div class="fio-contact-data">info@hsb.co.id</div>
-        </a>
+        <div class="footer-contact">
+          <a aria-label="Link Title" alt="link_title" class="footer-contact-link" href="tel:02150595588" target="_blank">
+            <div class="fio-contact-icon"><i class="fas fa-phone"></i></div>
+            <div class="fio-contact-data">+62 21 5059 5588</div>
+          </a>
+          <a aria-label="Link Title" alt="link_title" class="footer-contact-link" href="www.hsb.co.id" target="_blank">
+            <div class="fio-contact-icon"><i class="fas fa-globe"></i></div>
+            <div class="fio-contact-data">www.hsb.co.id</div>
+          </a>
+          <a aria-label="Link Title" alt="link_title" class="footer-contact-link" href="mailto:info@hsb.co.id" target="_blank">
+            <div class="fio-contact-icon"><i class="fas fa-envelope"></i></div>
+            <div class="fio-contact-data">info@hsb.co.id</div>
+          </a>
+        </div>
+        <div class="footer-socmed">
+          <a aria-label="Link Title" alt="link_title" class="footer-socmed-link fsl-fb" href="" target="_blank">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+          <a aria-label="Link Title" alt="link_title" class="footer-socmed-link fsl-tw" href="" target="_blank">
+            <i class="fab fa-twitter-square"></i>
+          </a>
+          <a aria-label="Link Title" alt="link_title" class="footer-socmed-link fsl-ig" href="" target="_blank">
+            <i class="fab fa-instagram-square"></i>
+          </a>
+          <a aria-label="Link Title" alt="link_title" class="footer-socmed-link fsl-wa" href="" target="_blank">
+            <i class="fab fa-whatsapp-square"></i>
+          </a>
+          <a aria-label="Link Title" alt="link_title" class="footer-socmed-link fsl-yt" href="" target="_blank">
+            <i class="fab fa-youtube-square"></i>
+          </a>
+          <a aria-label="Link Title" alt="link_title" class="footer-socmed-link fsl-ln" href="" target="_blank">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </div>
       </div>
+    </div>
+    <div class="footer-desc">
+      <p>Handal Semesta Berjangka memiliki izin operasional yang legal dan telah disahkan oleh Badan Pengawas Perdagangan Berjangka Komiditi (BAPPEBTI) Kementerian Perdagangan Republik Indonesia selaku Badan Otoritas Perdagangan Berjangka Komoditi dan Derivative di Indonesia.</p>
+      <p>Handal Semesta Berjangka juga merupakan suatu Perusahaan Pialang Berjangka yang resmi diakui oleh Pemerintah Republik Indonesia dan menjadi Anggota Bursa Indonesia Commodity & Derivatives Exchange (ICDX).</p>
+      <p>Handal Semesta Berjangka juga telah menjadi Anggota Kliring yang resmi dari Indonesia Clearing House (ICH) sebagai Lembaga Kliring (Penjamin).</p>
+    </div>
+    <div class="footer-risk">
+      <h5>PERINGATAN RISIKO</h5>
+      <p>Produk dengan leverage memiliki tingkat risiko yang tinggi terhadap modal yang anda investasikan dan disarankan hanya menggunakan dana yang mampu anda tanggung apabila terjadi kerugian. Nilai investasi dapat turun atau naik dan Anda dapat kehilangan pembayaran margin awal anda. Harap diketahui bahwa produk dengan leverage belum tentu cocok untuk semua orang, jadi pastikan anda telah memahami sepenuhnya semua risiko yang terlibat.</p>
+    </div>
+    <div class="footer-copyright content_center">
+      <h6>&copy; 2020 Handal Semesta Berjangka. Seluruh hak dilindungi undang-undang.</h6>
+    </div>
   </span>
 </footer>
