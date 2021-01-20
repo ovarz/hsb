97e8e0b35ab3d271b8e7abd0bb06ab0010c43b8c
ovarz <ovarz_03@yahoo.com>
2021-01-20 22:55:51 +0700

    finishing no effect

--- module/open.php
+++ module/open.php
@@ -1,16 +1,20 @@
-  <section class="open content_center">
-    <span>
-      <div class="section-title content_center">
-        <span>
-          <h3>Segera buka akun dan nikmati pengalaman trading terbaik</h3>
-        </span>
-      </div>
-      <div class="open-button content_center">
-        <a aria-label="Link Title" alt="link_title" class="btn" 
-        href="javascript:void(0)">
-          <div class="open-button-label">Buka akun demo</div>
-          <div class="open-button-desc">Dapatkan Dana Virtual $100.000</div>
-        </a>
-      </div>
-    </span>
-  </section>
+<section class="open content_center">
+  <div class="cover-image">
+    <div class="mobile-only flex_lazy"><img alt="Cover" data-original="img/open-mobile.jpg" /></div>
+    <div class="desktop-only flex_lazy"><img alt="Cover" data-original="img/open-desktop.jpg" /></div>
+  </div>
+  <span>
+    <div class="section-title content_center">
+      <span>
+        <h3>Segera buka akun dan nikmati pengalaman trading terbaik</h3>
+      </span>
+    </div>
+    <div class="open-button content_center">
+      <a aria-label="Link Title" alt="link_title" class="btn" 
+      href="javascript:void(0)">
+        <div class="open-button-label">Buka akun demo</div>
+        <div class="open-button-desc">Dapatkan Dana Virtual $100.000</div>
+      </a>
+    </div>
+  </span>
+</section>
--- module/win.php
+++ module/win.php
@@ -6,5 +6,40 @@
           <h4>HSB mempunyai promo trading yang menarik dan beragam, memberikan Anda bonus dan hadiah yang nyata</h4>
         </span>
       </div>
+      
+      <div class="win-content content_center">
+        <span>
+          <div class="win-content-box">
+            <div class="wcb-image flex_lazy">
+              <img alt="Promo" data-original="img/promo-luck.jpg" />
+            </div>
+            <div class="win-content-info">
+              <h5>Lucky Trader</h5>
+              <p class="wcb-highlight">Sudah 11.500 orang memenangkannya</p>
+              <p>Bonus hingga $70.000 menunggu Anda</p>
+            </div>
+          </div>
+          <div class="win-content-box">
+            <div class="wcb-image flex_lazy">
+              <img alt="Promo" data-original="img/promo-commission.jpg" />
+            </div>
+            <div class="win-content-info">
+              <h5>Promo komisi $0</h5>
+              <p class="wcb-highlight">Sudah 11.500 orang bergabung</p>
+              <p>Profit trading tak terbatas</p>
+            </div>
+          </div>
+          <div class="win-content-box">
+            <div class="wcb-image flex_lazy">
+              <img alt="Promo" data-original="img/promo-egg.jpg" />
+            </div>
+            <div class="win-content-info">
+              <h5>Promo Telur Emas</h5>
+              <p class="wcb-highlight">Sudah 11.500 orang memenangkannya</p>
+              <p>Ipad, emas batang dan hadiah lainnya. Peluang menang hingga 80%</p>
+            </div>
+          </div>
+        </span>
+      </div>
     </span>
   </section>
