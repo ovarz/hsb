db3c06a74b233700caaa66573baa186bc6bfc1e7
ovarz <ovarz_03@yahoo.com>
2021-01-20 17:25:29 +0700

    section open

--- module/open.php
+++ module/open.php
@@ -5,5 +5,12 @@
           <h3>Segera buka akun dan nikmati pengalaman trading terbaik</h3>
         </span>
       </div>
+      <div class="open-button content_center">
+        <a aria-label="Link Title" alt="link_title" class="btn" 
+        href="javascript:void(0)">
+          <div class="open-button-label">Buka akun demo</div>
+          <div class="open-button-desc">Dapatkan Dana Virtual $100.000</div>
+        </a>
+      </div>
     </span>
   </section>
