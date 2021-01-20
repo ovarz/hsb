e86c0466a1eea921f2939b7368e98f1944e58894
ovarz <ovarz_03@yahoo.com>
2021-01-20 21:02:18 +0700

    section profit

--- module/profit.php
+++ module/profit.php
@@ -6,5 +6,36 @@
           <h4>Informasi pasar yang tepat waktu, membantu Anda menangkap peluang trading</h4>
         </span>
       </div>
+      
+      <div class="profit-content content_center">
+        <span>
+          <div class="profit-content-box">
+            <div class="pcb-info">
+              <h5>Order transaksi pertama : EUR/USD</h5>
+            </div>
+            <div class="pcb-image flex_lazy">
+              <img alt="Content" data-original="img/eur-usd.jpg" />
+            </div>
+            <div class="pcb-info">
+              <h5>4/6 ECB mengumumkan resolusi suku bunga</h5>
+              <p>Sinyal HSB: Bank Sentral Eropa segera mengumumkan bahwa Pandemic Emergency Purchase Programme (PEPP) diperluas menjadi 600 miliar Euro, dan EUR/USD naik 155 poin dalam 5 jam</p>
+              <p>Ambil peluang trading: Trading 0.2 lot, profit $311</p>
+            </div>
+          </div>
+          <div class="profit-content-box">
+            <div class="pcb-info">
+              <h5>Order transaksi kedua : XAU/USD</h5>
+            </div>
+            <div class="pcb-image flex_lazy">
+              <img alt="Content" data-original="img/xau-usd.jpg" />
+            </div>
+            <div class="pcb-info">
+              <h5>5/6 Data Non-Farm Payroll lebih baik dari perkiraan</h5>
+              <p>Sinyal HSB: Pengumuman data Non-Farm Payroll lebih baik dari yang diharapkan, dolar naik, dan emas turun tajam $37</p>
+              <p>Ambil peluang trading: trading 0.1 lot, profit $370</p>
+            </div>
+          </div>
+        </span>
+      </div>
     </span>
   </section>
