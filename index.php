1eb8331e7e74876d88fdbcfdf9ac84cbb0530a1c
ovarz <ovarz_03@yahoo.com>
2021-01-21 19:49:23 +0700

    update cover

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <?php require ('inc/meta.php')?>
 <?php require ('inc/header.php')?>
 <div class="site-container">
-  <?php require ('module/cover.php')?>
+  <?php require ('module/highlight.php')?>
   <?php require ('module/character.php')?>
   <?php require ('module/reduce.php')?>
   <?php require ('module/profit.php')?>
---
+++ module/highlight.php
@@ -0,0 +1,40 @@
+<section class="highlight content_center">
+  <span>
+    <div class="highlight-image content_center flex_ori">
+      <img alt="Promo" data-original="img/highlight.png" />
+    </div>
+    <div class="highlight-info content_center">
+      <span>
+        <div class="highlight-title">
+          <h1>HSB Terdepan Dalam Solusi Trading</h1>
+          <h2>Pilih platform trading yang telah diunduh jutaan orang</h2>
+        </div>
+        <div class="highlight-feature">
+          <div class="highlight-feature-list">
+            <div class="hfl-icon content_center"><i class="fad fa-hand-holding-usd"></i></div>
+            <div class="hfl-info content_center"><span>Komisi $0</span></div>
+          </div>
+          <div class="highlight-feature-list">
+            <div class="hfl-icon content_center"><i class="fad fa-cctv"></i></div>
+            <div class="hfl-info content_center"><span>Diawasi BAPPEBTI</span></div>
+          </div>
+          <div class="highlight-feature-list">
+            <div class="hfl-icon content_center"><i class="fad fa-info-circle"></i></div>
+            <div class="hfl-info content_center"><span>Informasi pasar dan keuangan</span></div>
+          </div>
+          <div class="highlight-feature-list">
+            <div class="hfl-icon content_center"><i class="fad fa-user-headset"></i></div>
+            <div class="hfl-info content_center"><span>Layanan CS 7 x 24 jam</span></div>
+          </div>
+        </div>
+        <div class="highlight-button">
+          <a aria-label="Link Title" alt="link_title" class="btn" 
+          href="javascript:void(0)">
+            <div class="highlight-button-label">Buka Akun Sekarang</div>
+            <div class="highlight-button-desc">Dapatkan Dana Virtual $100.000</div>
+          </a>
+        </div>
+      </span>
+    </div>
+  </span>
+</section>
